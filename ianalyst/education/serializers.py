2ceeadb93923941e8bfa373b6291d0060d223888
Nikita Mashtakov <nik_1612@mail.ru>
2023-05-10 23:38:22 +0300

    Добавленые 3 преставления, связанные с сериализаторами

--- ianalyst/education/serializers.py
+++ ianalyst/education/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Lesson
+from .models import Lesson, Block
+
 
 class EducationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ('id', 'block', 'lesson_name')
+
+
+class BlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Block
+        fields = "__all__"
--- ianalyst/education/views.py
+++ ianalyst/education/views.py
@@ -1,10 +1,20 @@
 from rest_framework import generics
 from django.shortcuts import render
-from .models import Lesson
-from .serializers import EducationSerializer
+from .models import Lesson, Block
+from .serializers import EducationSerializer, BlockSerializer
 
 
-# Create your views here.
-class EducationAPIView(generics.ListAPIView):
+# queryset - возвращает список записей клиенту
+class EducationAPIView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = EducationSerializer
+
+
+class BlockAPIView(generics.ListCreateAPIView):
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
+
+
+class BlockAPICrud (generics.RetrieveUpdateDestroyAPIView):
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
